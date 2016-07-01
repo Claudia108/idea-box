2b87f4cd66de38c6e7a7ee8f4c96963a0af3abba
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-01 13:15:00 -0600

    Remove enum from quality

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,11 +1,35 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  test "should have a quality that defaults to 0" do
+  test "should have a quality that defaults to swill" do
     idea = Idea.new
     assert_equal("swill", idea.quality)
   end
 
+  test "it is valid with a quality of swill" do
+  ideas(:one).quality = "swill"
+
+  assert(ideas(:one).valid?)
+  end
+
+  test "it is valid with a quality of plausible" do
+    ideas(:one).quality = "plausible"
+
+    assert(ideas(:one).valid?)
+  end
+
+  test "it is valid with a quality of genius" do
+    ideas(:one).quality = "genius"
+
+    assert(ideas(:one).valid?)
+  end
+
+  test "it is invalid with any other quality" do
+    ideas(:one).quality = "invalid"
+
+    refute(ideas(:one).valid?)
+  end
+  
   test "it should be invalid without a title or body" do
     idea_without_title_or_body = Idea.new
 
